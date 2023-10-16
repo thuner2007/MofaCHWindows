927fd52acb6645f8d70f47684666cda530a1bc04
Colin Walker <thuner2007@outlook.com>
2023-10-16 19:24:03 +0200

    livechat ui looking better

--- src/liveChat.js
+++ src/liveChat.js
@@ -7,10 +7,78 @@ import {
   query,
   orderBy,
 } from "./firestore.js";
-import { auth } from "./auth.js";
 import { user } from "./main.js";
 
+let loggedIn = false;
+
 // Live chat related functions go here...
+
+document.getElementById("liveChatBtn").addEventListener("click", (e) => {
+  loggedIn = true;
+  onSnapshot(
+    query(collection(db, "LiveChat"), orderBy("timestamp")),
+    (querySnapshot) => {
+      const LiveChatMessagesLeft = document.getElementById(
+        "LiveChatMessagesLeft"
+      );
+      const LiveChatMessagesRight = document.getElementById(
+        "LiveChatMessagesRight"
+      );
+
+      // Clear any existing content in the container
+      LiveChatMessagesLeft.innerHTML = "";
+      LiveChatMessagesRight.innerHTML = "";
+
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        const message = data.message;
+        const username = data.username;
+        const timestamp = data.timestamp;
+        const email = data.email;
+
+        // Create a new <p> element for each message
+        const newPTag = document.createElement("p");
+        newPTag.textContent = username + ": " + message;
+        if (email === user.email) {
+          newPTag.classList.add("myText");
+          LiveChatMessagesRight.appendChild(newPTag);
+        } else {
+          newPTag.classList.add("othersText");
+          LiveChatMessagesLeft.appendChild(newPTag);
+        }
+
+        const date = new Date(timestamp);
+
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1; // Month is 0-indexed, so add 1
+        const day = date.getDate();
+
+        const formattedDate = `${year}-${month
+          .toString()
+          .padStart(2, "0")}-${day.toString().padStart(2, "0")}`;
+
+        const timestampText = document.createElement("p");
+        timestampText.textContent = formattedDate;
+        if (email === user.email) {
+          timestampText.classList.add("timestampright");
+        } else {
+          timestampText.classList.add("timestampleft");
+        }
+
+        newPTag.appendChild(timestampText);
+
+        const divElement = document.getElementById("LiveChatMessagesLeft");
+        divElement.scrollTop = divElement.scrollHeight;
+        const divElement2 = document.getElementById("LiveChatMessagesRight");
+        divElement2.scrollTop = divElement2.scrollHeight;
+      });
+    }
+  );
+});
+
+document.getElementById("logoutProfile").addEventListener("click", (e) => {
+  loggedIn = false;
+});
 
 function makeid(length) {
   let result = "";
@@ -30,33 +98,10 @@ document.getElementById("sendMessage").addEventListener("click", (e) => {
 
   setDoc(doc(db, "LiveChat", makeid(10)), {
     username: user.email,
+    email: user.email,
     message: document.getElementById("messageInput").value,
     timestamp: timestamp, // Set the timestamp in the document
   });
 
   document.getElementById("messageInput").value = "";
 });
-
-onSnapshot(
-  query(collection(db, "LiveChat"), orderBy("timestamp")),
-  (querySnapshot) => {
-    const liveChatContainer = document.getElementById("LiveChatMessages");
-
-    // Clear any existing content in the container
-    liveChatContainer.innerHTML = "";
-
-    querySnapshot.forEach((doc) => {
-      const data = doc.data();
-      const message = data.message;
-      const username = data.username;
-      const timestamp = data.timestamp;
-
-      // Create a new <p> element for each message
-      const newPTag = document.createElement("p");
-      newPTag.textContent = username + ": " + message;
-
-      // Append the <p> element to the container
-      liveChatContainer.appendChild(newPTag);
-    });
-  }
-);
